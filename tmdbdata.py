7f62e1bd9a33c66027285548d4b211a8a8b62e61
gillesf <gilles.fevry@ensae.fr>
2024-12-29 11:13:21 +0000

    comments on functions

--- tmdbdata.py
+++ tmdbdata.py
@@ -11,6 +11,7 @@ headers = {"accept": "application/json",
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjcwNTY1MTA0YzUzY2Q0MjE3N2M3ZWQyMmVmZDk1ZCIsIm5iZiI6MTczMjI5MzAwNy4yMTk0MDM1LCJzdWIiOiI2NzQwYWY5OWRkNTNmYTAwNjAzMzM3YzUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.8DPhrUjEakZO4CxeovXkNmSOe01nH3r5kEZlntq547M"
             }
 
+#The genre dictionnary provided by the API.
 genre_dictionnary =requests.get('https://api.themoviedb.org/3/genre/movie/list?language=en', headers=headers).json()['genres']
 
 def get_movie_ids_list(nb_pages=1, headers=None):
@@ -24,6 +25,7 @@ def get_movie_ids_list(nb_pages=1, headers=None):
         Returns:
         list: list of movie ids
     """
+
     assert headers is not None, "Headers is not None"
     assert isinstance(nb_pages, int) and nb_pages >= 1, "nb_pages is not an int >=1"
 
@@ -54,6 +56,7 @@ def get_movies_info(ids=[], headers=None):
         Returns:
         A Pandas data frame
     """
+
     assert headers is not None, "Headers is not None"
     assert len(ids)>=1, "List is empty"
 
@@ -86,6 +89,7 @@ def get_balanced_movie_list(nb_pages=1, headers=None):
         Returns:
         list: list of movie ids
     """
+
     assert headers is not None, "Headers is not None"
     assert isinstance(nb_pages, int) and nb_pages >= 1, "nb_pages is not an int >=1"
 
@@ -111,11 +115,23 @@ def get_balanced_movie_list(nb_pages=1, headers=None):
     return(ids)
 
 def clean_data(df=None):
-    if  "status_message" in df.columns:
+    """
+        Calls a series of functions to clean the data-frame provided with get_movies_info
+
+        Parameters:
+        df: a pandas data-frame created with get_movies_info
+
+        Returns:
+        df1: a cleaned data-frame
+    """
+
+    assert df is not None, "No data frame was given"
+
+    if  "status_message" in df.columns: #get rid of ligns where there was a status_error (=no data could be rethrieved)
         df1=df[df["status_message"].isna()]
     else:
         df1=df.copy()
-    df1=df1.dropna(subset=['overview'])
+    df1=df1.dropna(subset=['overview']) #get rid of ligns where there is no overview
     df1=drop_useless_info(df1)
     df1=keep_main_genre(df1)
     df1=full_poster_path(df1)
@@ -123,18 +139,30 @@ def clean_data(df=None):
     df1=transform_date(df1)
     return(df1)
 
-def drop_useless_info(df=None):
+def drop_useless_info(df=None, drop_original_title=True):
+    """
+        Drops useless info
+
+        Parameters:
+        df: a pandas data-frame created with get_movies_info
+        drop_original_title: a Boolean
+
+        Returns:
+        df1: a cleaned data-frame
+    """
+
     assert df is not None, "No data frame was given"
 
     # Liste des colonnes à supprimer
     columns_to_drop = [
         "adult", "backdrop_path", "belongs_to_collection", "homepage", "imdb_id", 
-        "origin_country", "original_language", "original_title", "production_companies", 
+        "origin_country", "original_language", "production_companies", "success",
         "production_countries", "spoken_languages", "status", "tagline", "video", 
-        "belongs_to_collection.id", "belongs_to_collection.name", 
-        "belongs_to_collection.poster_path", "belongs_to_collection.backdrop_path","success","status_code", "status_message"
-
+        "belongs_to_collection.id", "belongs_to_collection.name", "status_code",
+        "belongs_to_collection.poster_path", "belongs_to_collection.backdrop_path", "status_message"
     ]
+    if drop_original_title:
+        columns_to_drop.extend(["original_title"])
 
     # Vérifier quelles colonnes sont présentes avant de les supprimer
     columns_in_df = [col for col in columns_to_drop if col in df.columns]
@@ -145,11 +173,20 @@ def drop_useless_info(df=None):
     return df1
 
 def keep_main_genre(df=None):
+    """
+        Changes the way genre is encoded
 
-    if isinstance(df['genres'].iloc[0], str):
+        Parameters:
+        df: a pandas data-frame where 'genre' is a list of genre dictionnaries 
+
+        Returns:
+        df1: a cleaned data-frame with no 'genre' but a 'main_genre_id' and a 'main_genre_name' column
+    """
+
+    if isinstance(df['genres'].iloc[0], str): #in case we open a csv flight where the dictionnaries were transformed in str.
         df['genres'] = df['genres'].apply(ast.literal_eval)
     df = df.dropna(subset=['genres'])
-    df = df[df['genres'].apply(lambda x: len(x) > 0)]
+    df = df[df['genres'].apply(lambda x: len(x) > 0)] #get rid of ligns ith no genre (encoded: "")
 
     df['main_genre_id'] = df['genres'].map(lambda x: x[0]['id'])
     df['main_genre_name'] = df['genres'].map(lambda x: x[0]['name'])
@@ -157,18 +194,47 @@ def keep_main_genre(df=None):
     return(df1)
 
 def full_poster_path(df=None):
+    """
+        Changes the way the poster path is encoded
+
+        Parameters:
+        df: a pandas data-frame whith a 'poster_path' column
+
+        Returns:
+        df1: a pandas data frame with no 'poster_path' column but a 'full_poster_path' column containing the fulle poster url
+    """
 
     df['full_poster_path']= "https://image.tmdb.org/t/p/original/"+df['poster_path']
     df1=df.drop(columns=["poster_path"])
     return(df1)
 
 def count_words(df=None):
+    """
+        Changes the way the poster path is encoded
+
+        Parameters:
+        df: a pandas data-frame whith a 'poster_path' column
+
+        Returns:
+        df1: a pandas data frame with no 'poster_path' column but a 'full_poster_path' column containing the fulle poster url
+    """
+
     df1=df.copy()
     df1["overview_count"]=df1["overview"].str.len()
     df1["title_count"]=df1["title"].str.len()
     return(df1)
 
 def transform_date(df=None):
+    """
+        Changes the way the date is encoded
+
+        Parameters:
+        df: a pandas data-frame whith a 'release_date' column
+
+        Returns:
+        df1: a pandas data frame with a 'timestamp' column: the number of seconds between 1970 and the movie release date. 
+    """
+
     df['release_date'] = pd.to_datetime(df['release_date'])
     df['timestamp'] = df['release_date'].astype('int64') // 10**9
     return(df)
